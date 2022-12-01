331a842ef2af1669ffc223a631910251fc0e6369
Juan Jesus Mata Solis <jujemataso@ittepic.edu.mx>
2022-12-01 13:18:38 -0700

    ENVIO DE SMS

--- app/src/main/java/mx/edu/ittepic/ladm_u4_practica1_almacen_sms/Mensaje.kt
+++ app/src/main/java/mx/edu/ittepic/ladm_u4_practica1_almacen_sms/Mensaje.kt
@@ -1,5 +1,7 @@
 package mx.edu.ittepic.ladm_u4_practica1_almacen_sms
 
+import android.telephony.SmsManager
+
 data class Mensaje(val destinatario: String, val fecha: String, val mensaje: String, val id: String?) {
     override fun toString(): String {
         return "Destinatario: $destinatario\n$mensaje\n$fecha"
@@ -11,5 +13,10 @@ data class Mensaje(val destinatario: String, val fecha: String, val mensaje: Str
             "mensaje" to mensaje,
             "fecha" to fecha
         )
+    }
+
+    fun enviar(){
+        val smsManager = SmsManager.getDefault()
+        smsManager.sendTextMessage(destinatario, null, mensaje, null, null)
     }
 }
--- app/src/main/java/mx/edu/ittepic/ladm_u4_practica1_almacen_sms/NuevoMensaje.kt
+++ app/src/main/java/mx/edu/ittepic/ladm_u4_practica1_almacen_sms/NuevoMensaje.kt
@@ -1,8 +1,12 @@
 package mx.edu.ittepic.ladm_u4_practica1_almacen_sms
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import mx.edu.ittepic.ladm_u4_practica1_almacen_sms.databinding.ActivityNuevoMensajeBinding
 import java.util.*
@@ -12,6 +16,7 @@ class NuevoMensaje : AppCompatActivity() {
     val operacionesFirebase = OperacionesFirebase()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        revisarPermisos()
         binding = ActivityNuevoMensajeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -19,9 +24,17 @@ class NuevoMensaje : AppCompatActivity() {
         binding.cancelar.setOnClickListener { finish() }
     }
 
+    private fun revisarPermisos() {
+        val permiso = this.baseContext.checkSelfPermission(Manifest.permission.SEND_SMS)
+        if(permiso != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(arrayOf(Manifest.permission.SEND_SMS), 200)
+        }
+    }
+
     private fun enviarMensaje(mensaje: Mensaje) {
         operacionesFirebase.guardarMensaje(mensaje)
             .addOnSuccessListener {
+                mensaje.enviar()
                 Toast.makeText(this, "MENSAJE ENVIADO", Toast.LENGTH_LONG).show()
                 finish()
             }
